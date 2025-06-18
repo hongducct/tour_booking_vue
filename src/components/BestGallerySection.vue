b757bc74cd1edcc29e685d8de34bd7d6e611bf7f
hongducct <duchongkhoan100@gmail.com>
2025-06-18 16:52:55 +0700

    update ảnh chia sẻ homepage

--- src/components/BestGallerySection.vue
+++ src/components/BestGallerySection.vue
@@ -265,16 +265,16 @@ import { RouterLink, useRouter } from 'vue-router'
 // Gallery Images
 const galleryImages = [
   {
-    src: 'https://cdn.bookingtour.vn/thumb_x315x250/upload/admin/2024/09/25/anh_dep_tour_thai_lan3-19075.jpg',
-    alt: 'Ảnh đẹp Tour Thái Lan 3',
+    src: 'https://res.cloudinary.com/dlhra4ihw/image/upload/v1750240312/ny2mltajalsvizdidngt.jpg',
+    alt: 'Ảnh đẹp Tour Cửa Lò - Trinh Nguyễn chia sẻ',
   },
   {
-    src: 'https://cdn.bookingtour.vn/thumb_x315x250/upload/admin/2024/09/25/anh_dep_tour_thai_lan1-19071.jpg',
-    alt: 'Ảnh đẹp Tour Thái Lan 1',
+    src: 'https://res.cloudinary.com/dlhra4ihw/image/upload/v1750239499/yhygawagnhwsk4kih08g.jpg',
+    alt: 'Ảnh đẹp Tour Sapa - Hồng Đức chia sẻ',
   },
   {
-    src: 'https://cdn.bookingtour.vn/thumb_x655x350/upload/admin/2024/09/25/anh_dep_tour_thai_lan2-19075.jpg',
-    alt: 'Ảnh đẹp Tour Thái Lan 2',
+    src: 'https://res.cloudinary.com/dlhra4ihw/image/upload/v1750240086/rudlshtb7gdktzx7scin.jpg',
+    alt: 'Ảnh đẹp Tour Nha Trang - Trinh Nguyễn chia sẻ',
   },
 ]
 
